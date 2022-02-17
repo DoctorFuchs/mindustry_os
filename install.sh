5d8907297f76f7084340c852ac4e123c3ff14b74
DoctorFuchs <fuchs9203@gmail.com>
2022-02-17 17:28:49 +0100

    Projektdateien hinzufügen.

---
+++ install.sh
@@ -0,0 +1,8 @@
+cd ~
+
+sudo apt install git python3
+python3 -m pip install requests httpie
+
+git clone 
+
+echo "python3 ~/loader.py" >> ~/.bashrc
---
+++ loader.py
@@ -0,0 +1,35 @@
+import requests
+import subprocess
+import os
+
+ACCEPT_PRERELEASE = False
+
+res = requests.get("https://api.github.com/repos/Anuken/Mindustry/releases").json() 
+
+home = os.path.expanduser("~")
+
+RELEASE = {}
+
+print("searching for releases")
+
+for release in res:
+    is_prerelease = release.get("prerelease")
+    if ACCEPT_PRERELEASE == is_prerelease:
+        RELEASE = release
+
+print("found release -> "+RELEASE.get("name"))
+file_name = home + RELEASE.get("tag_name").replace(".", "_")+".jar"
+if not os.path.isfile(file_name):
+    for assets in RELEASE.get("assets"):
+        if assets.get("name").startswith("server"):
+            print("server file found")
+            subprocess.call(["https", "--follow", "-o", file_name, assets.get("browser_download_url")])
+            break
+    print("finished download")
+
+else: 
+    print("already up to date")
+
+print("starting server...")
+subprocess.call(["java", "-jar", file_name])
+subprocess.call(["sudo", "poweroff"])
