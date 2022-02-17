f98213c1c48583cfb5a9124ea2d7026e2e2b3802
DoctorFuchs <fuchs9203@gmail.com>
2022-02-17 17:36:04 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
-cd ~
+cd ~/mindustry_os
 
 sudo apt install git python3
 python3 -m pip install requests httpie
 
-git clone 
-
-echo "python3 ~/loader.py" >> ~/.bashrc
+echo "git pull https://github.com/DoctorFuchs/mindustry_os.git" >> ~/.bashrc
+echo "python3 ~/mindustry_os/loader.py" >> ~/.bashrc
