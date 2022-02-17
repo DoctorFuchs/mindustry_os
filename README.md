94dd7765255180fdc4a187c54471268af5fefd0d
DoctorFuchs <fuchs9203@gmail.com>
2022-02-17 17:54:37 +0100

    java install

---
+++ README.md
@@ -0,0 +1,7 @@
+# install
+
+```bash
+git clone https://github.com/DoctorFuchs/mindustry_os.git # don't download it from github!
+source mindustry_os/install.sh
+# restart your pi and it will work
+```
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~/mindustry_os
 
-sudo apt install git python3
+sudo apt install git default-jdk python3 -y 
 python3 -m pip install requests httpie
 
 echo "git pull https://github.com/DoctorFuchs/mindustry_os.git" >> ~/.bashrc
