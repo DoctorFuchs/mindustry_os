5dac43d5f69fe0a9ab6ef5b76f7271e0e1cbf113
DoctorFuchs <fuchs9203@gmail.com>
2022-02-17 18:18:38 +0100

    bug fix 2

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-cd ~/mindustry_os
-
 sudo apt install git default-jdk python3 python3-pip -y 
 python3 -m pip install requests httpie
 
+echo "cd ~/mindustry_os"
 echo "git pull https://github.com/DoctorFuchs/mindustry_os.git" >> ~/.bashrc
 echo "python3 ~/mindustry_os/loader.py" >> ~/.bashrc
