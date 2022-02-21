ff38141e011221b02434ce87b8ab6d730bffc6e8
DoctorFuchs <fuchs9203@gmail.com>
2022-02-21 21:06:11 +0100

    fix

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install git default-jdk python3 python3-pip -y 
 python3 -m pip install requests httpie
 
-echo "source ~/mindustry_os/loader.sh"
+echo "source ~/mindustry_os/loader.sh" >> ~/.bashrc
