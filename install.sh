f835a8ed95fab5f86653961900c6a4a9bd435756
DoctorFuchs <fuchs9203@gmail.com>
2022-02-21 21:04:30 +0100

    better update possibilities

--- install.sh
+++ install.sh
@@ -2,6 +2,4 @@ sudo apt update
 sudo apt install git default-jdk python3 python3-pip -y 
 python3 -m pip install requests httpie
 
-echo "cd ~/mindustry_os" >> ~/.bashrc
-echo "git pull https://github.com/DoctorFuchs/mindustry_os.git" >> ~/.bashrc
-echo "sudo python3 ~/mindustry_os/loader.py" >> ~/.bashrc
+echo "source ~/mindustry_os/loader.sh"
---
+++ loader.sh
@@ -0,0 +1,3 @@
+cd ~/mindustry_os
+git pull -q
+sudo python3 ~/mindustry_os/loader.py
