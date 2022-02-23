84914da63bf9814bb3d7d8b3de43a7fc64ff8c37
DoctorFuchs <fuchs9203@gmail.com>
2022-02-23 18:10:44 +0000

    after work

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt install git default-jdk python3 python3-pip -y 
-python3 -m pip install requests httpie
+python3 -m pip install requests
 
 echo "source ~/mindustry_os/loader.sh" >> ~/.bashrc
--- loader.sh
+++ loader.sh
@@ -1,3 +1,5 @@
+read "Start Mindustry server [Press ctrl-C to exit]?"
+
 cd ~/mindustry_os
 git pull -q
 sudo python3 ~/mindustry_os/loader.py
